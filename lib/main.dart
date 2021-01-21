2e52754ba104086e9fa83c4183147114e23f31ef
noelvillaman <namalliv2@hotmail.com>
2021-01-21 13:51:18 -0600

    Added two other widgets to the app.

--- lib/main.dart
+++ lib/main.dart
@@ -14,36 +14,38 @@ class MyApp extends StatefulWidget {
 }
 
 class MyAppState extends State<MyApp> {
-  var questionState = 0;
+  var _questionState = 0;
+  final questions = const [
+    {"questionText" : "What is your favorite color?",
+      "answers" : ["Black", "Red", "Green", "White"],
+    },
+    {"questionText" : "What is your favorite animal?",
+      "answers" : ["Lion", "Cat", "Dog", "Elephant"],
+    },
+    {"questionText" : "Who is your favorite player?",
+      "answers" : ["Trout", "Pujols", "Jordan", "Melo"],
+    },
+  ];
   void questionAnswer(){
+
     setState(() {
-      if(questionState < 3) {
-        questionState++;
+      if(_questionState < questions.length) {
+        _questionState++;
       }
     });
     print("Noel won!");
   }
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {"questionText" : "What is your favorite color?",
-      "answers" : ["Black", "Red", "Green", "Whte"],
-      },
-      {"questionText" : "What is your favorite animal?",
-        "answers" : ["Lion", "Cat", "Dog", "Elephant"],
-      },
-      {"questionText" : "Who is your favorite player?",
-        "answers" : ["Trout", "PUjols", "Jordan", "Melo"],
-      },
-    ];
     return MaterialApp(home: Scaffold(
       appBar: AppBar(title: Text("Flutter 2021"),),
-      body: Column(children: [
-        Question(questions[questionState]["questionText"]),
-        ...(questions[questionState]["answers"] as List<String>).map((answer){
+      body: _questionState < questions.length ?
+      Column(children: [
+        Question(questions[_questionState]["questionText"]),
+        ...(questions[_questionState]["answers"] as List<String>).map((answer){
           return Answer(questionAnswer, answer);
         }).toList()
-      ],)
-    ),);//("This is Noels first app with Flutter 01 18 2021!"),);
+      ],) : Center(child: Text("All done!"))
+    ),);
   }
 }
